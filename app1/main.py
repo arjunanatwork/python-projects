05462052fa4dfd6e72b4f4abf25c1198d9daa32a
Arjunan Ravi <arjunanatwork@gmail.com>
2024-11-10 12:56:15 +0400

    Add edit functionality to todo list
    Enhanced the todo list program by introducing an edit feature. Users can now edit existing todos by specifying the number of the todo they want to modify. This makes the application more versatile and user-friendly.

--- app1/main.py
+++ app1/main.py
@@ -1,13 +1,18 @@
 todos = []
 
 while True:
-    user_action = input("Type add or show:")
+    user_action = input("Type add, show, edit or exit:")
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
         case 'show':
             print(todos)
+        case 'edit':
+            number = int(input("Number of the todo to edit:"))
+            number = number - 1
+            new_todo = input("Enter new todo:")
+            todos[number] =  new_todo
         case 'exit':
             break
 
