644d0a77e1ae1200159d71a802c3ed925175b229
Arjunan Ravi <arjunanatwork@gmail.com>
2024-11-13 19:41:49 +0400

    Add 'complete' functionality to todo list
    Updated main loop to include a 'complete' case, allowing users to remove tasks by their number. Enhanced 'show' case to display todos with their index for better clarity.

--- app1/main.py
+++ app1/main.py
@@ -1,18 +1,23 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit or exit:")
+    user_action = input("Type add, show, edit, complete or exit:")
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
         case 'show':
-            print(todos)
+            for index, item in enumerate(todos):
+                row = f"{index+1}-{item}"
+                print(row)
         case 'edit':
             number = int(input("Number of the todo to edit:"))
             number = number - 1
             new_todo = input("Enter new todo:")
             todos[number] =  new_todo
+        case 'complete':
+            number = int(input("Number of the todo to complete:"))
+            todos.pop(number-1)
         case 'exit':
             break
 
