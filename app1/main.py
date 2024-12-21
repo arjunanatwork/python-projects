a2300c4d8aeca0aec3d118f8f846feb6e4d8c78d
Arjunan Ravi <arjunanatwork@gmail.com>
2024-12-21 20:54:27 +0400

    Refactor file reading with a reusable get_todos function.
    Introduced a `get_todos` function to streamline reading todos from the file. Used the new function to replace repetitive file reading logic across multiple actions, improving code clarity and reducing redundancy. Also added new todos ("Make soup" and "Make pizza") to the `todos.txt` file.

--- app1/main.py
+++ app1/main.py
@@ -1,4 +1,7 @@
-
+def get_todos():
+    with open('todos.txt', 'r') as file:
+        todos = file.readlines()
+    return todos
 
 while True:
     user_action = input("Type add, show, edit, complete or exit:")
@@ -7,16 +10,15 @@ while True:
     if user_action.startswith('add'):
         todo = user_action[4:]
 
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+        todos = get_todos()
 
         todos.append(todo + '\n')
 
         with open('todos.txt', 'w') as file:
             file.writelines(todos)
     elif user_action.startswith('show'):
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+
+        todos = get_todos()
 
         # new_todos = [item.strip('\n') for item in todos]
 
@@ -28,8 +30,7 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+            todos = get_todos()
 
             new_todo = input("Enter new todo:")
             todos[number] =  new_todo + '\n'
@@ -43,8 +44,7 @@ while True:
         try:
             number = int(user_action[9:])
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+            todos = get_todos()
 
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
