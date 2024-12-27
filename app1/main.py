db4ef0401d8a3dc12fe7cd8830cdfe47a93cfc82
Arjunan Ravi <arjunanatwork@gmail.com>
2024-12-27 05:55:24 +0400

    Refactor default arguments and update todos.txt
    Set default filepath arguments in `get_todos` and `write_todos` functions to simplify code. Adjusted main.py to remove redundant filepath specification. Updated todos.txt to replace "Make pizza" with "Make bread".

--- app1/main.py
+++ app1/main.py
@@ -1,9 +1,13 @@
-def get_todos(filepath):
+def get_todos(filepath="todos.txt"):
+    """ Read a text file amd return the list of
+    to-do items
+    """
     with open(filepath, 'r') as file:
         todos = file.readlines()
     return todos
 
-def write_todos(filepath, todos_arg):
+def write_todos(todos_arg, filepath="todos.txt"):
+    """ Write a to-d- item in the text file"""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
@@ -14,15 +18,15 @@ while True:
     if user_action.startswith('add'):
         todo = user_action[4:]
 
-        todos = get_todos('todos.txt')
+        todos = get_todos()
 
         todos.append(todo + '\n')
 
-        write_todos('todos.txt', todos)
+        write_todos(todos)
 
     elif user_action.startswith('show'):
 
-        todos = get_todos('todos.txt')
+        todos = get_todos()
 
         # new_todos = [item.strip('\n') for item in todos]
 
@@ -34,12 +38,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todos('todos.txt')
+            todos = get_todos()
 
             new_todo = input("Enter new todo:")
             todos[number] =  new_todo + '\n'
 
-            write_todos('todos.txt', todos)
+            write_todos(todos)
 
         except ValueError:
             print("Your command is not valid.")
@@ -48,13 +52,13 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos('todos.txt')
+            todos = get_todos()
 
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(number-1)
 
-            write_todos('todos.txt', todos)
+            write_todos(todos)
 
             message = f"Todo {todo_to_remove} was removed from the list"
             print(message)
