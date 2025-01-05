79f298da9d6e54062500a456d4e702e8462ffd0b
Arjunan Ravi <arjunanatwork@gmail.com>
2025-01-05 17:02:29 +0400

    Add timestamp in main.py and refactor filepath handling
    Added a timestamp display in `main.py` to show the current date and time. Refactored `functions.py` to use a constant `FILEPATH` for default file paths, improving maintainability.

--- app1/functions.py
+++ app1/functions.py
@@ -1,4 +1,6 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
     """ Read a text file amd return the list of
     to-do items
     """
@@ -6,7 +8,7 @@ def get_todos(filepath="todos.txt"):
         todos = file.readlines()
     return todos
 
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     """ Write a to-d- item in the text file"""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
