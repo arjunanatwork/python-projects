ad4f409dd2a4a17f7c271b64af18508df2ca9252
Arjunan Ravi <arjunanatwork@gmail.com>
2025-02-15 18:10:10 +0400

    Refactor GUI logic and enhance to-do functionality.
    Updated `gui.py` to add event handling for adding to-dos and closing the window, along with improved font styling. Corrected typo in `functions.py` docstring and added a new to-do item in `todos.txt`.

--- app1/functions.py
+++ app1/functions.py
@@ -9,6 +9,6 @@ def get_todos(filepath=FILEPATH):
     return todos
 
 def write_todos(todos_arg, filepath=FILEPATH):
-    """ Write a to-d- item in the text file"""
+    """ Write a to-do item in the text file"""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
