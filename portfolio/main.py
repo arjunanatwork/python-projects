01153cdff1aea9ed96b05db0959b1a77d8ad36dd
Arjunan Ravi <arjunanatwork@gmail.com>
2025-03-24 09:14:34 +0400

    Add main.py with personal portfolio layout using Streamlit
    Introduce a Streamlit-based layout for displaying a personal portfolio. Includes an image, title, and descriptive content showcasing experience, skills, and interests.

---
+++ portfolio/main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Arjunan Ravi")
+    content="""
+    Versatile Full Stack Developer with 9+ years of experience in analysis, design, implementation and support in large scale high performance applications. Technically ambitious and always looking for new challenges. Highly Adaptable to change, always willing to explore, experiment newer domains & technologies. ❤️ building apps. I have few other passions: Video Games 🎮, Music 🎵 and Travel ✈️.
+    """
+    st.write(content)
