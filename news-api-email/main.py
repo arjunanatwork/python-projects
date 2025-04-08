cdebe479b2c5bd00b6de294e1238c097cfb3fbaf
Arjunan Ravi <arjunanatwork@gmail.com>
2025-04-08 05:18:11 +0400

    Refactor API response handling to parse JSON content
    Replaced text-based response processing with JSON parsing for better structure and data accessibility. Added iteration to extract and print article titles and descriptions from the returned data. This enhances the usability and clarity of the retrieved information.

--- news-api-email/main.py
+++ news-api-email/main.py
@@ -4,5 +4,13 @@ api_key = "f183118abb91490abac45a53f170110a"
 url = ("https://newsapi.org/v2/everything?q=tesla&"
        "from=2025-03-08&sortBy=publishedAt&apiKey=f183118abb91490abac45a53f170110a")
 
+# Make the request
 request = requests.get(url)
-content = request.text
+
+# Get a dictionary with data
+content = request.json()
+
+# Access the article titles and description
+for article in content['articles']:
+    print(article['title'])
+    print(article['description'])
