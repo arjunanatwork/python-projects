aa77831741f93618a7e1f05030e414568e466aab
Arjunan Ravi <arjunanatwork@gmail.com>
2025-04-10 09:10:50 +0400

    Refactor API response handling to parse JSON content
    Replaced text-based response processing with JSON parsing for better structure and data accessibility. Added iteration to extract and print article titles and descriptions from the returned data. This enhances the usability and clarity of the retrieved information.

--- news-api-email/main.py
+++ news-api-email/main.py
@@ -1,5 +1,7 @@
 import requests
 
+from send_email import send_email
+
 api_key = "f183118abb91490abac45a53f170110a"
 url = ("https://newsapi.org/v2/everything?q=tesla&"
        "from=2025-03-08&sortBy=publishedAt&apiKey=f183118abb91490abac45a53f170110a")
@@ -11,6 +13,10 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article titles and description
+body= ""
 for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+    if article['title'] is not None and article['description'] is not None:
+        body = body + article['title'] + "\n" + article['description'] + "\n\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
---
+++ news-api-email/send_email.py
@@ -0,0 +1,14 @@
+import smtplib
+import ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "arjunanatwork@gmail.com"
+    password = "zyly hnta rutz oegf"
+    receiver = "arjunanatwork@gmail.com"
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
