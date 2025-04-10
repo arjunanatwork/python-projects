63e9a7e3879b7027341ab4983fdb07b126acba38
Arjunan Ravi <arjunanatwork@gmail.com>
2025-04-10 09:48:57 +0400

    Update news fetching script with dynamic date and formatting
    Replaced hardcoded date with dynamic current date to ensure up-to-date results. Improved email content structure by adding subject and article URLs. Limited the number of articles processed to 20 for efficiency.

--- news-api-email/main.py
+++ news-api-email/main.py
@@ -1,10 +1,15 @@
 import requests
-
+from datetime import datetime
 from send_email import send_email
 
+topic = "tesla"
+current_date = datetime.now().strftime("%Y-%m-%d")
+
 api_key = "f183118abb91490abac45a53f170110a"
-url = ("https://newsapi.org/v2/everything?q=tesla&"
-       "from=2025-03-08&sortBy=publishedAt&apiKey=f183118abb91490abac45a53f170110a")
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&from={current_date}&"
+       "sortBy=publishedAt&"
+       "apiKey=f183118abb91490abac45a53f170110a&language=en")
 
 # Make the request
 request = requests.get(url)
@@ -14,9 +19,9 @@ content = request.json()
 
 # Access the article titles and description
 body= ""
-for article in content['articles']:
+for article in content['articles'][:20]:
     if article['title'] is not None and article['description'] is not None:
-        body = body + article['title'] + "\n" + article['description'] + "\n\n"
+        body = "Subject: Today's news" + "\n" + body + article['title'] + "\n" + article['description'] + "\n" + article["url"] + 2*"\n"
 
 body = body.encode("utf-8")
 send_email(message=body)
