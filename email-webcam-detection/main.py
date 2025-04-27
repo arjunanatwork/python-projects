a3b63c41eba5815763627fb14707f4deec51d6ac
Arjunan Ravi <arjunanatwork@gmail.com>
2025-04-27 18:00:07 +0400

    Add motion detection with contour highlighting
    Introduced motion detection by processing frames with grayscale conversion, Gaussian blur, and thresholding. Contours are identified and highlighted with rectangles, and "Movement" status is displayed for areas exceeding a size threshold. This improves video analysis functionality significantly.

--- email-webcam-detection/main.py
+++ email-webcam-detection/main.py
@@ -4,10 +4,32 @@ import time
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
+first_frame = None
+
 while True:
     check, frame = video.read()
-    cv2.imshow("Video", frame)
+    grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    grey_frame_gau = cv2.GaussianBlur(grey_frame, (21,21), 0)
 
+    if first_frame is None:
+        first_frame = grey_frame_gau
+
+    delta_frame = cv2.absdiff(first_frame, grey_frame_gau)
+
+    thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
+    cv2.imshow("My Video", dil_frame)
+
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        cv2.putText(frame, "Status: {}".format("Movement"), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+
+    cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
 
     if key == ord('q'):
