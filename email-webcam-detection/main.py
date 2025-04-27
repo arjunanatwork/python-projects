9b6ecff9827210b883ea431d2e07cbb77af28719
Arjunan Ravi <arjunanatwork@gmail.com>
2025-04-27 19:02:36 +0400

    Add motion detection and email alert system
    Integrated a motion detection mechanism that captures images of moving objects. Implemented email notifications with attachments when movement stops, ensuring prompt alerts with relevant images.

--- email-webcam-detection/main.py
+++ email-webcam-detection/main.py
@@ -1,12 +1,24 @@
+import glob
+import os
 import cv2
 import time
+from emailing import send_email
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
+status_list = []
+count = 1
+image_with_object = None
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
 
 while True:
+    status = 0
     check, frame = video.read()
     grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     grey_frame_gau = cv2.GaussianBlur(grey_frame, (21,21), 0)
@@ -26,8 +38,23 @@ while True:
         if cv2.contourArea(contour) < 5000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        if rectangle.any():
+            status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count += 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
         cv2.putText(frame, "Status: {}".format("Movement"), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+
+    status_list.append(status)
+    status_list = status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1] == 0:
+        send_email(image_with_object)
+        clean_folder()
+
 
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
