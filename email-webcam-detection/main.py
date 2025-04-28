f56bbb3aa3976da630fd3dcf03a95ec8a35fdc75
Arjunan Ravi <arjunanatwork@gmail.com>
2025-04-28 20:59:58 +0400

    Use threads for email sending and folder cleanup
    Replaced direct calls to `send_email` and `clean_folder` with threads to improve application responsiveness. Threads ensure non-blocking execution, enhancing performance during video processing.

--- email-webcam-detection/main.py
+++ email-webcam-detection/main.py
@@ -3,6 +3,7 @@ import os
 import cv2
 import time
 from emailing import send_email
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
@@ -52,8 +53,12 @@ while True:
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email(image_with_object)
-        clean_folder()
+        email_thread = Thread(target=send_email, args=(image_with_object,))
+        email_thread.daemon = True
+        cleanup_thread = Thread(target=clean_folder)
+        cleanup_thread.daemon = True
+        email_thread.start()
+
 
 
     cv2.imshow("Video", frame)
@@ -63,5 +68,5 @@ while True:
         break
 
 video.release()
-
+cleanup_thread.start()
 
