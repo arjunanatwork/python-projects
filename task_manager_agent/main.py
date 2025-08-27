6139f993cc7dabc3814c2b6eae66ca74c062b82a
Arjunan Ravi <arjunanatwork@gmail.com>
2025-08-27 21:32:13 +0400

    Add task management tool and integrate with agent executor
    Introduced a new `add_task` tool for handling task creation and integrated it into the `AgentExecutor` for improved task management. Replaced the previous chain invocation logic with the agent-based approach to enhance functionality and maintainability. Updated the prompt setup to include `MessagesPlaceholder` for better flexibility.

--- task_manager_agent/main.py
+++ task_manager_agent/main.py
@@ -3,28 +3,44 @@ import os
 
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.tools import tool
+from langchain.agents import create_openai_tools_agent, AgentExecutor
 
 load_dotenv()
 
 todoist_api_key = os.getenv("TODOIST_API_KEY")
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 
+@tool
+def add_task(task):
+    """Add a new task to the user's task list. Use this when the user wants to add or create a new task."""
+    print(task)
+    print("Task added")
+
+tools = [add_task]
+
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash",
                              google_api_key=gemini_api_key,
                              temperature=0.3)
 
 system_prompt = "You are a helpful assistant. You will help the user add tasks."
-user_input = "What day is it today?"
+user_input = "add a new task to buy a new car"
 
 prompt = ChatPromptTemplate.from_messages([
     ("system", system_prompt),
-    ("user", user_input)
+    ("user", user_input),
+    MessagesPlaceholder("agent_scratchpad")
 ])
 
-chain = prompt | llm | StrOutputParser()
+# chain = prompt | llm | StrOutputParser()
+agent = create_openai_tools_agent(llm, tools, prompt)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=False)
 
-response = chain.invoke({"input": user_input})
+
+# response = chain.invoke({"input": user_input})
+
+response = agent_executor.invoke({"input": user_input})
 
 print(response)
