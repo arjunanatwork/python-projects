2240fb2984b7f004d2cc8c92240eab6f44ac8211
Arjunan Ravi <arjunanatwork@gmail.com>
2025-08-29 14:35:40 +0400

    Add Todoist integration for task management
    Introduced Todoist API to support adding and displaying tasks. Enhanced the assistant to handle user queries for adding tasks with optional descriptions and retrieving all tasks in a bulleted format. Updated the interactive flow to maintain conversation history.

--- task_manager_agent/main.py
+++ task_manager_agent/main.py
@@ -7,30 +7,43 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.tools import tool
 from langchain.agents import create_openai_tools_agent, AgentExecutor
+from todoist_api_python.api import TodoistAPI
 
 load_dotenv()
 
 todoist_api_key = os.getenv("TODOIST_API_KEY")
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 
-@tool
-def add_task(task):
-    """Add a new task to the user's task list. Use this when the user wants to add or create a new task."""
-    print(task)
-    print("Task added")
+todoist_api = TodoistAPI(todoist_api_key)
 
-tools = [add_task]
+@tool
+def add_task(task, desc=None):
+    """Add a new task to the user's task list. Use this when the user wants to add or create a new task."""
+    todoist_api.add_task(content=task, description=desc)
+
+@tool
+def show_task():
+    """Show all tasks from Todoist. Use this tool when user wants to see all tasks."""
+    result_paginator = todoist_api.get_tasks()
+    tasks = []
+    for task_list in result_paginator:
+        for task in task_list:
+            tasks.append(task)
+    return tasks
+
+tools = [add_task, show_task]
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash",
                              google_api_key=gemini_api_key,
                              temperature=0.3)
 
-system_prompt = "You are a helpful assistant. You will help the user add tasks."
-user_input = "add a new task to buy a new car"
+system_prompt = """You are a helpful assistant. You will help the user add tasks. You will help the user show existing tasks.
+                 If the user asks to show the tasks: for example, "show me the tasks" print out the tasks to the user in a bulleted format"""
 
 prompt = ChatPromptTemplate.from_messages([
     ("system", system_prompt),
-    ("user", user_input),
+    MessagesPlaceholder("history"),
+    ("user", "{input}"),
     MessagesPlaceholder("agent_scratchpad")
 ])
 
@@ -41,6 +54,11 @@ agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=False)
 
 # response = chain.invoke({"input": user_input})
 
-response = agent_executor.invoke({"input": user_input})
+history = []
+while True:
+    user_input = input("You: ")
+    response = agent_executor.invoke({"input": user_input, "history": history})
+    print(response['output'])
+    history.append(HumanMessage(content=user_input))
+    history.append(AIMessage(content=response['output']))
 
-print(response)
